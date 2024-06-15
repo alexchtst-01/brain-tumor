c159098b9d238db4722b6c41d5d92a5299bc5439
alexchtst-01 <acinatra@gmail.com>
2024-06-16 00:38:00 +0700

    feat: repair the train and validation function

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -72,8 +72,8 @@ class CreateAnotationLayers:
         else:
             print("configurasi data tidak sesuai")
 
-testingMigration = CreateAnotationLayers(src_path="raw_data/test", destination_path="data/test")
-testingMigration.createAnotation()
+# testingMigration = CreateAnotationLayers(src_path="raw_data/test", destination_path="data/test")
+# testingMigration.createAnotation()
 
-trainingMigration = CreateAnotationLayers(src_path="raw_data/train", destination_path="data/train", anot_key='id')
-trainingMigration.createAnotation()
+# trainingMigration = CreateAnotationLayers(src_path="raw_data/train", destination_path="data/train", anot_key='id')
+# trainingMigration.createAnotation()
