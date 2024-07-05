2f7f42642d2eb0cbd6d700399d225cffea73628e
alexchtst-01 <acinatra@gmail.com>
2024-07-05 18:12:22 +0700

    more proper datapre processing for coco dataset

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -8,14 +8,13 @@ import cv2 as cv
 
 class CreateAnotationLayers:
     def __init__(self, src_path, destination_path, anot_key='image_id'):
-        self.__showWarning()
         self.src_path = src_path
         self.destination_path = destination_path
         self.data = self.generate_data()
         self.images = self.data['images']
         self.anotations = self.data['annotations']
         self.anot_key = anot_key
-        self.__image_segmentations = []
+        self.imagepath_points_list = []
     
     def generate_data(self):
         for i in os.listdir(self.src_path):
@@ -27,70 +26,97 @@ class CreateAnotationLayers:
         
         return data
     
-    def __showWarning(self):
-        print("make sure your surce and destination like below: ")
-        print("source ")
-        print("  --there is coco.json ")
-        print("  --and the images ")
-        print("destination ")
-        print("  --images ")
-        print("  --masks ")
-        print("  --view ")
-    
     def __plugAnotations(self):
-        for image in self.images:
+        for image_data in self.images:
             lst = []
-            for anotation in self.anotations:
-                if image['id'] == anotation[self.anot_key]:
-                    lst.append({
-                        'file_name': image['file_name'],
-                        'class': anotation['category_id'],
-                        'width': image['width'],
-                        'height': image['height'],
-                        'points': anotation['segmentation']
-                    })
-            self.__image_segmentations.append(lst)
+            for anot_data in self.anotations:
+                if anot_data[self.anot_key] == image_data['id']:
+                    lst.append(
+                        {
+                            'file_name': image_data['file_name'],
+                            'class': anot_data['category_id'],
+                            'width': image_data['width'],
+                            'height': image_data['height'],
+                            'segmentation': anot_data['segmentation']
+                        }
+                    )
+            self.imagepath_points_list.append(lst)
         
         return True
     
-    def createAnotation(self, migrate_all_files=False):
+    
+    def maskingLayer(self):
         if self.__plugAnotations():
-            for segment_properties in tqdm(self.__image_segmentations):
-                img = cv.imread(f"{self.src_path}/{segment_properties[0]['file_name']}")
-                width = segment_properties[0]['width']
-                height = segment_properties[0]['width']
+            image_mask = []
+            for segment_properties in self.imagepath_points_list:
                 
+                class_list = []
+                for i in range(6):
+                    class_list.append(np.zeros(shape=(segment_properties[0]['width'], segment_properties[0]['height']), dtype=np.uint8))
+                    
+                for segment_item in segment_properties:
+                    points = np.array(segment_item['segmentation'])[0].reshape(-1, 2)
+                    points = points.astype(np.int32)
+                    
+                    cv.fillPoly(class_list[segment_item['class']], [points], color=(255, 255, 255))
+                    
+                image_mask.append(class_list)
+                print(f"done creating mask in f{segment_properties[0]['file_name']}")
+            image_mask = np.array(image_mask)
+            
+            return image_mask
+            
+    
+    def createAnotation(self, migrate_files=False):
+        if self.__plugAnotations():
+            for data in self.imagepath_points_list:
+                img = cv.imread(f"{self.src_path}/{data[0]['file_name']}")
+                width = data[0]['width']
+                height = data[0]['height']
                 mask = np.zeros(shape=(width, height, 3), dtype=np.uint8)
                 
-                for segment_item in segment_properties:
-                    points = np.array(segment_item['points'])[0].reshape(-1, 1, 2)
+                for item in data:
+                    
+                    points = np.array(item['segmentation'])[0].reshape(-1, 1, 2)
                     points = points.astype(np.int32)
                     
                     cv.polylines(mask, [points], isClosed=True, color=((255, 0, 0)), thickness=1)
                     
-                    if segment_item['class'] == 0 or segment_item['class']:
+                    if item['class'] == 0 or item['class']:
                         cv.fillPoly(mask, [points], color=((255, 0, 0)))
-                    if segment_item['class'] == 2:
+                    if item['class'] == 2:
                         cv.fillPoly(mask, [points], color=((255, 255, 0)))
-                    if segment_item['class'] == 3:
+                    if item['class'] == 3:
                         cv.fillPoly(mask, [points], color=((255, 255, 255)))
-                    if segment_item['class'] == 4:
+                    if item['class'] == 4:
                         cv.fillPoly(mask, [points], color=((0, 255, 0)))
-                    if segment_item['class'] == 5:
+                    if item['class'] == 5:
                         cv.fillPoly(mask, [points], color=((255, 0, 0)))
                     
+                    print(item['class'])
                     cv.polylines(img, [points], isClosed=True, color=((0, 255, 255)), thickness=2)
-                    
-                mask_name = f"{self.destination_path}/masks/mask_{segment_properties[0]['file_name']}"
-                view_name = f"{self.destination_path}/view/view_{segment_properties[0]['file_name']}"
+                
+                
+                mask_name = f"{self.destination_path}/masks/mask_{data[0]['file_name']}"
+                view_name = f"{self.destination_path}/view/view_{data[0]['file_name']}"
                     
                 plt.imsave(f"{mask_name}", mask, cmap='gray')
                 plt.imsave(f"{view_name}", img, cmap='gray')
                 
-                if migrate_all_files:
-                    shutil.move(f"{self.src_path}/{segment_item['file_name']}", f"{self.destination_path}/images/{segment_item['file_name']}")
+                
+                if migrate_files:
+                    shutil.move(f"{self.src_path}/{item['file_name']}", f"{self.destination_path}/images/{item['file_name']}")
                 else:
-                    shutil.copy(f"{self.src_path}/{segment_item['file_name']}", f"{self.destination_path}/images/{segment_item['file_name']}")
+                    shutil.copy(f"{self.src_path}/{item['file_name']}", f"{self.destination_path}/images/{item['file_name']}")
         else:
-            print("ya kodenya error, kasian")
+            print("ya ga kekonfig")
+            
+            
 
+def main():
+    testing = CreateAnotationLayers(src_path="test_baru", destination_path="raw_data/test")
+    mask = testing.maskingLayer()
+    print(mask.shape)
+
+if __name__ == "__main__":
+    main()
